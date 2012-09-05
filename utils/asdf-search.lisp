a1a3b620b7c4d8241c78aa23994c3fe8693bc36c
pnathan <pnathan@vandals.uidaho.edu>
2012-09-05 00:05:10 -0700

    ASDF can be converted into a graph given a starting root package.
    This work is to allow isolating blockages in the cl-test-grid.
    
    Next up is to enumerate all ASDF packages and generate the total ASDF graph.

--- utils/asdf-search.lisp
+++ utils/asdf-search.lisp
@@ -1,30 +1,74 @@
-(let ((graph (cl-graph:make-graph 'cl-graph:graph-container)))
-  (cl-graph:add-vertex graph (asdf:find-system :batteries)
-                       :if-duplicate-do :ignore)
-  (cl-graph:add-vertex graph (asdf:find-system :defobject)
-                       :if-duplicate-do :ignore)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; pnathan 2012. experimental work to build a utility for finding
+;;; broken systems in cl-test-grid.
 
-  graph)
+;; base dependency.
+(ql:quickload :cl-graph)
+
+;; unit tests
+(ql:quickload :fiveam)
+(use-package :fiveam)
 
 
+(defun find-asdf-depends (system)
+  "Returns the list of the asdf dependencies of system"
+  ;; Rest of.
+  (mapcar #'make-keyword
+  (cdr
+   ;; second componenent
+   (second
+    (asdf:component-depends-on
+     'asdf:load-op
+     system)))))
+
+(defun make-keyword (str)
+  (intern (string-upcase str) "KEYWORD"))
 
 (defun add-system-to (graph system)
-  (format t "adding vertex ~a~% "  (asdf:find-system system))
+  "Adds ASDF system to graph.
 
-  (cl-graph:add-vertex
-   graph (asdf:find-system system)
-   :if-duplicate-do :ignore)
+The system should be specified as a keyword
 
-  (let ((depends
-	 (cdr (second
-	       (asdf:component-depends-on 'asdf:load-op (asdf:find-system system))))))
-    (when depends
-      (list depends
-	    (loop for comp in depends
-	       ;; its possible nconc is ok here.
-	       collect (add-system-to graph comp))))))
+Mutates state"
+
+  (let* ((asdf-system-keyword
+	  (make-keyword (asdf:component-name (asdf:find-system system)))))
+    (unless (cl-graph:find-vertex graph asdf-system-keyword nil)
+
+      (format t "adding vertex ~a~% "  asdf-system-keyword)
+
+      (cl-graph:add-vertex graph
+        asdf-system-keyword
+			   ;; We should have found this one
+			   ;; already. Error! Error!
+			   :if-duplicate-do :error)
+
+      (format t "~a:  ~A~&" system  (find-asdf-depends (asdf:find-system system)))
+      (loop for dep in (find-asdf-depends (asdf:find-system system))
+	 do
+	   (format t "adding edge between ~a and ~a~&"
+		   asdf-system-keyword dep)
+
+	   (add-system-to graph dep)
+	   (cl-graph:add-edge-between-vertexes graph asdf-system-keyword dep)))))
 
 (defun find-all-asdf-depending-for-load (system )
   (let ((graph (cl-graph:make-graph 'cl-graph:graph-container)))
     (add-system-to graph system)
   graph))
+
+
+;;; experimental/workbook code
+(cl-graph:graph->dot
+ (find-all-asdf-depending-for-load :cl-yahoo-finance)
+ "~/asdf-dump.dot"
+ :edge-labeler #'(lambda (e stream)
+		   ;; don't output anything
+		   (format stream ""))
+ :vertex-labeler
+ #'(lambda (v stream)
+       (format t "~a~&" (cl-graph:element v))
+       (format stream "~a" (cl-graph:element v))
+       ))
+
+(asdf:find-system (make-keyword (asdf:component-name (asdf:find-system :spatial-trees))))
